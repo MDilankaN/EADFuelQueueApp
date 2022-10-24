2aa733a09324e94b9828f3e166b7e002008d7140
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2022-10-25 02:08:50 +0530

    implemented privacy and policy ui

--- app/src/main/java/com/example/myapplication/LoginUI.java
+++ app/src/main/java/com/example/myapplication/LoginUI.java
@@ -9,38 +9,9 @@ import android.widget.Button;
 
 public class LoginUI extends AppCompatActivity {
 
-    private Button privacy, contactus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_ui);
-
-        privacy = findViewById(R.id.button2);
-        contactus = findViewById(R.id.button3);
-
-        privacy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openNewActivity();
-            }
-        });
-
-        contactus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openNewActivity2();
-            }
-        });
-
-    }
-
-    public void openNewActivity(){
-        Intent intent = new Intent(this, PrivacyAndPolicy.class);
-        startActivity(intent);
-    }
-
-    public void openNewActivity2(){
-        Intent intent = new Intent(this, ContactUs.class);
-        startActivity(intent);
     }
 }
