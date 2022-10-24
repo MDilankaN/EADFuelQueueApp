33b4897ae30a8d3d072e943dfad0919e458d27f3
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-10-25 01:52:58 +0530

    Login Screen UI

--- app/src/main/java/com/example/myapplication/LoginUI.java
+++ app/src/main/java/com/example/myapplication/LoginUI.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginUI extends AppCompatActivity {
+
+    Button LoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_ui);
+
+        LoginBtn = findViewById(R.id.login_btn);
+
+//        LoginBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent =  new Intent(LoginUI.this, HomeUI.class);
+//                startActivity(intent);
+//            }
+//        });
+
+
     }
+
 }
