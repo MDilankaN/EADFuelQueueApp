fc541227aff549de1c88d7c9faeca8d803af0288
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-10-25 13:41:52 +0530

    Home UI added with fragments

--- app/src/main/java/com/example/myapplication/HomeUI.java
+++ app/src/main/java/com/example/myapplication/HomeUI.java
@@ -1,14 +1,45 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeUI extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_ui);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home_menu_icon:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,homeFragment).commit();
+                        return true;
+                    case R.id.settings_menu_id:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,settingsFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
 }
