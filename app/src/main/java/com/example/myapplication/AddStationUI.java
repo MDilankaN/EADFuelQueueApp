fceb4ffee192b9f406c82d697258f0661d050329
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2022-10-25 21:44:26 +0530

    added opening and closing time clocks

--- app/src/main/java/com/example/myapplication/AddStationUI.java
+++ app/src/main/java/com/example/myapplication/AddStationUI.java
@@ -2,13 +2,79 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class AddStationUI extends AppCompatActivity {
 
+    private ImageButton btnOpenTime, btnCloseTime;
+    private TextView TimeTextView1, TimeTextView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_station_ui);
+
+        btnOpenTime = findViewById(R.id.btn_opening_time);
+        btnCloseTime = findViewById(R.id.btn_closing_time);
+
+        TimeTextView1 = findViewById(R.id.textView_open_time);
+        TimeTextView2 = findViewById(R.id.textView_closing_time);
+
+        //https://youtu.be/suMdH7WSewg
+        btnOpenTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar=Calendar.getInstance();
+                int hours=calendar.get(Calendar.HOUR_OF_DAY);
+                int mins=calendar.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog= new TimePickerDialog(AddStationUI.this, androidx.appcompat.R.style.Theme_AppCompat_Dialog, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Calendar c=Calendar.getInstance();
+                        c.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        c.set(Calendar.MINUTE,minute);
+                        c.setTimeZone(TimeZone.getDefault());
+                        SimpleDateFormat format= new SimpleDateFormat("k:mm a");
+                        String time = format.format(c.getTime());
+                        TimeTextView1.setText(time);
+                    }
+                },hours ,mins,false);
+                timePickerDialog.show();
+            }
+        });
+
+        btnCloseTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar=Calendar.getInstance();
+                int hours=calendar.get(Calendar.HOUR_OF_DAY);
+                int mins=calendar.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog= new TimePickerDialog(AddStationUI.this, androidx.appcompat.R.style.Theme_AppCompat_Dialog, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Calendar c=Calendar.getInstance();
+                        c.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        c.set(Calendar.MINUTE,minute);
+                        c.setTimeZone(TimeZone.getDefault());
+                        SimpleDateFormat format= new SimpleDateFormat("k:mm a");
+                        String time = format.format(c.getTime());
+                        TimeTextView2.setText(time);
+                    }
+                },hours ,mins,false);
+                timePickerDialog.show();
+            }
+        });
+
+
     }
+
+
 }
