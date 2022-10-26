785020863e8334d6e211c4d321b3056e5c438848
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-10-26 22:27:52 +0530

    Join and Leave from QueueList shows

--- app/src/main/java/com/example/myapplication/StationPageUI.java
+++ app/src/main/java/com/example/myapplication/StationPageUI.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.api.JasonPlaceHolderAPI;
@@ -15,6 +16,10 @@ import com.example.myapplication.models.QueueList;
 import com.example.myapplication.models.User;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.annotations.SerializedName;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,9 +29,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class StationPageUI extends AppCompatActivity {
 
+    TextView ListItem;
     Button joinQueue, exitBeforeBtn, exitAfterBtn;
     private JasonPlaceHolderAPI jsonPlaceHolderAPI;
     QueueList CurrentUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,16 +43,24 @@ public class StationPageUI extends AppCompatActivity {
         exitBeforeBtn = findViewById(R.id.btn_exit_before);
         exitAfterBtn = findViewById(R.id.btn_exit_after);
 
-        exitBeforeBtn.setVisibility( View.GONE);
-        exitAfterBtn.setVisibility( View.GONE);
+        ListItem = findViewById(R.id.list_itemname);
+
+
+        exitBeforeBtn.setVisibility(View.GONE);
+        exitAfterBtn.setVisibility(View.GONE);
 
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://ead-backend-fuel-queue.herokuapp.com").addConverterFactory(GsonConverterFactory.create()).build();
         jsonPlaceHolderAPI = retrofit.create(JasonPlaceHolderAPI.class);
 
+        GetListInQueue("11");
+
         joinQueue.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View view) {
-                joinList("11","11","11","11","1");
+                Date currentTime = Calendar.getInstance().getTime();
+                joinList("11", "11", currentTime.toString(), "11", "1");
+                GetListInQueue("11");
             }
         });
 
@@ -64,27 +79,51 @@ public class StationPageUI extends AppCompatActivity {
         });
     }
 
+    private void GetListInQueue(String id) {
+        Call<List<QueueList>> call = jsonPlaceHolderAPI.QueueListOrder(id.trim());
+        call.enqueue(new Callback<List<QueueList>>() {
+            @Override
+            public void onResponse(Call<List<QueueList>> call, Response<List<QueueList>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(StationPageUI.this, "Error", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                List<QueueList> queueList = response.body();
+
+                for (QueueList queueListI : queueList) {
+                    String content = "ID: " + queueListI.getId() + "\n" + "ID: " + queueListI.getUserID() + "\n" + "ID: " + queueListI.getJoinTime() + "\n\n\n";
+                    System.out.println(content);
+                    ListItem.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<QueueList>> call, Throwable t) {
+                Toast.makeText(StationPageUI.this, "Error : onFailure", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
     public void searchStation(String searchValue) {
 
         System.out.println(searchValue);
 
     }
 
-    public void openNewActivity(){
+    public void openNewActivity() {
         Intent intent = new Intent(this, AddStationUI.class);
         startActivity(intent);
     }
 
-    public void openNewActivity2(){
+    public void openNewActivity2() {
         Intent intent = new Intent(this, UpdateStationUI.class);
         startActivity(intent);
     }
 
 
-
-
     public void joinList(String UserID, String QueueID, String JoinTime, String LeftTime, String Position) {
-        QueueList list = new QueueList(UserID, QueueID, JoinTime,LeftTime, Position);
+        QueueList list = new QueueList(UserID, QueueID, JoinTime, LeftTime, Position);
         System.out.println(list.getId());
         System.out.println("--------------------------------");
         Call<QueueList> call = jsonPlaceHolderAPI.joinQueueList(list);
@@ -102,9 +141,9 @@ public class StationPageUI extends AppCompatActivity {
                 CurrentUser = joinres;
                 System.out.println("joinres");
                 System.out.println(joinres.getId());
-                exitBeforeBtn.setVisibility( View.VISIBLE);
-                exitAfterBtn.setVisibility( View.VISIBLE);
-                joinQueue.setVisibility( View.GONE);
+                exitBeforeBtn.setVisibility(View.VISIBLE);
+                exitAfterBtn.setVisibility(View.VISIBLE);
+                joinQueue.setVisibility(View.GONE);
             }
 
             @Override
@@ -114,7 +153,7 @@ public class StationPageUI extends AppCompatActivity {
         });
     }
 
-    public void RemoveFromList(String ID){
+    public void RemoveFromList(String ID) {
         Call<Void> call = jsonPlaceHolderAPI.RemoveFromList(ID);
         call.enqueue(new Callback<Void>() {
             @Override
@@ -124,9 +163,9 @@ public class StationPageUI extends AppCompatActivity {
                     return;
                 }
 
-                exitBeforeBtn.setVisibility( View.GONE);
-                exitAfterBtn.setVisibility( View.GONE);
-                joinQueue.setVisibility( View.VISIBLE);
+                exitBeforeBtn.setVisibility(View.GONE);
+                exitAfterBtn.setVisibility(View.GONE);
+                joinQueue.setVisibility(View.VISIBLE);
                 Toast.makeText(StationPageUI.this, "Exited From the List", Toast.LENGTH_LONG).show();
             }
 
