13aa13200e89dc07c5e39f42da843a6e6f226736
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2022-10-26 13:06:23 +0530

    completed QueueList model

--- app/src/main/java/com/example/myapplication/models/QueueList.java
+++ app/src/main/java/com/example/myapplication/models/QueueList.java
@@ -16,4 +16,44 @@ public class QueueList {
     @SerializedName("position")
     private String Position;
 
+    public String getId() {
+        return Id;
+    }
+
+    public String getUserID() {
+        return UserID;
+    }
+
+    public String getQueueID() {
+        return QueueID;
+    }
+
+    public String getJoinTime() {
+        return JoinTime;
+    }
+
+    public String getLeftTime() {
+        return LeftTime;
+    }
+
+    public String getPosition() {
+        return Position;
+    }
+
+    public QueueList(String userID, String queueID, String joinTime, String leftTime, String position) {
+        UserID = userID;
+        QueueID = queueID;
+        JoinTime = joinTime;
+        LeftTime = leftTime;
+        Position = position;
+    }
+
+    public QueueList(String id, String userID, String queueID, String joinTime, String leftTime, String position) {
+        Id = id;
+        UserID = userID;
+        QueueID = queueID;
+        JoinTime = joinTime;
+        LeftTime = leftTime;
+        Position = position;
+    }
 }
