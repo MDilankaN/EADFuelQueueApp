d78729db5ba3d8b78b9cbc108f7af5f89effa114
Sithpavan Giridaran <68677837+Sidpavn@users.noreply.github.com>
2022-10-26 09:21:29 +0530

    added queue crud

---
+++ app/src/main/java/com/example/myapplication/QueueCrudUI.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class QueueCrudUI extends AppCompatActivity {
+
+    private Button add_queue, update_queue, remove_queue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_queue_crud_ui);
+
+        add_queue = findViewById(R.id.btn_addqueue);
+        update_queue = findViewById(R.id.btn_updatequeue);
+        remove_queue = findViewById(R.id.btn_removequeue);
+
+        add_queue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewActivity();
+            }
+        });
+
+        update_queue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewActivity2();
+            }
+        });
+
+        remove_queue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, AddQueueUI.class);
+        startActivity(intent);
+    }
+
+    public void openNewActivity2(){
+        Intent intent = new Intent(this, UpdateQueueUI.class);
+        startActivity(intent);
+    }
+}
