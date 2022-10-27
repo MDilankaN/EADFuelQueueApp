aa661cc2580ae18a7e4f467bd9380917637fd114
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2022-10-27 23:42:47 +0530

    search added

--- app/src/main/java/com/example/myapplication/StaffCrudUI.java
+++ app/src/main/java/com/example/myapplication/StaffCrudUI.java
@@ -6,18 +6,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.api.JasonPlaceHolderAPI;
+import com.example.myapplication.models.Queue;
+import com.example.myapplication.models.User;
 import com.google.android.material.snackbar.Snackbar;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class StaffCrudUI  extends AppCompatActivity {
 
     Button addStaffBtn, updateStaffBtn, removeStaffBtn, searchBtn;
     EditText search;
-    String searchVal;
-
+    TextView result;
+    String searchVal,id, username, email, empNo, password;
+    LinearLayout result_box;
+    private JasonPlaceHolderAPI jsonPlaceHolderAPI;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +40,10 @@ public class StaffCrudUI  extends AppCompatActivity {
         addStaffBtn = findViewById(R.id.addStaffBtn);
         updateStaffBtn = findViewById(R.id.updateStaffBtn);
         removeStaffBtn = findViewById(R.id.removeStaffBtn);
-
+        result_box = findViewById(R.id.searchResultBox);
         searchBtn = findViewById(R.id.searchStationBtn);
         search = findViewById(R.id.staffField);
+        result = findViewById(R.id.result);
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +51,8 @@ public class StaffCrudUI  extends AppCompatActivity {
                 searchVal = search.getText().toString();
 
                 if (!searchVal.equals("")) {
+                    Retrofit retrofit = new Retrofit.Builder().baseUrl("https://ead-backend-fuel-queue.herokuapp.com").addConverterFactory(GsonConverterFactory.create()).build();
+                    jsonPlaceHolderAPI = retrofit.create(JasonPlaceHolderAPI.class);
                     searchStaff(searchVal);
                 } else {
                     Snackbar.make(v, "Fields are Empty", Snackbar.LENGTH_SHORT).show();
@@ -70,6 +87,47 @@ public class StaffCrudUI  extends AppCompatActivity {
     public void searchStaff(String searchValue) {
 
         System.out.println(searchValue);
+        Call<User> call = jsonPlaceHolderAPI.getUserByID(searchValue);
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(StaffCrudUI.this, "Not Found", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                result_box.setVisibility(View.VISIBLE);
+                updateStaffBtn.setVisibility(View.VISIBLE);
+                removeStaffBtn.setVisibility(View.VISIBLE);
 
+                User user = response.body();
+                id = user.getId();
+                username = user.getUserName();
+                email = user.getEmail();
+                empNo = user.getVehicleNo();
+                password = user.getPassword();
+
+                String content = "";
+                result.setText(content);
+                content += "User ID: " + user.getId() + "\n";
+                content += "User name: " + user.getUserName() + "\n";
+                content += "Email: " + user.getEmail() + "\n";
+                content += "Emp no: " + user.getVehicleNo() + "\n";
+                content += "Password: " + user.getPassword() + "\n";
+//                if(queues.getFuelStatus() == true){
+//                    content += "Fuel Status: Available";
+//                }
+//                if(queues.getFuelStatus() == false){
+//                    content += "Fuel Status: Unavailable";
+//                }
+                result.append(content);
+                Toast.makeText(StaffCrudUI.this, "Result Found", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                System.out.println(t);
+                Toast.makeText(StaffCrudUI.this, "Error: Failed", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
